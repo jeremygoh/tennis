dd1e38d599dedc67e119402365ccf75b553400d1
Jeremy Goh <jg485host@gmail.com>
2013-07-16 21:09:55 +0100

    working version with games, tiebreaks,sets and matches. Needs to be refactored

--- spec/javascripts/GameSpec.js
+++ spec/javascripts/GameSpec.js
@@ -2,8 +2,8 @@
 describe ("Game", function() {
 
 	beforeEach(function(){
-		player1 = new Player();
-		player2 = new Player();
+		player1 = new Player("female");
+		player2 = new Player("female");
 		game = new Game(player1, player2);
 	});
 
@@ -172,8 +172,142 @@ it("should return 1-0 when a player has won six games in a row", function(){
 	
 })
 
+it("should not return 1-0 for sets, games should be 6-5", function(){
+	player1.games = 5;
+	player2.games = 5;
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	expect(game.sets()).not.toEqual("1-0");
+	expect(game.games()).toEqual("6-5"); //6-5
+})
+
+it("should allow a 7-5 victory to give a set of 1-0", function()
+{	
+	player1.games = 6;
+	player2.games = 5;
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	expect(game.sets()).toEqual("1-0");
+})
+
+xit("should not win a set if it is 6-6 and then a player wins two games", function(){
+	player1.games = 6;
+	player2.games = 6;
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	game.winPoint(player1);
+	expect(game.sets()).not.toEqual("1-0");
+})
+
+it("should trigger a tiebreak if it is 6-6 and a player wins a point", function(){
+	player1.games = 6;
+	player2.games = 6;
+	game.winPoint(player1);
+	expect(player1.score).toEqual(0);
+	expect(game.tiebreak()).toEqual("1-0");
+})
+
+it("should add a set when the tiebreak is 7-0, tiebreak should reset to 0", function(){
+	player1.games = 6;
+	player2.games = 6;
+	player1.tiebreak=6;
+	game.winPoint(player1);
+	expect(game.sets()).toEqual("1-0");
+	expect(player1.tiebreak).toEqual(0);
+
+})
+
+it("shouldn't add a set when the tiebreak is 7-6", function(){
+	player1.games = 6;
+	player2.games = 6;
+	player1.tiebreak = 6;
+	player2.tiebreak = 6;
+	game.winPoint(player1);
+	expect(game.sets()).not.toEqual("1-0");
+})
+
+it("should add a set when a tiebreak is won 11-9", function(){
+	player1.games = 6;
+	player2.games = 6;
+	player1.tiebreak = 9;
+	player2.tiebreak = 9;
+	game.winPoint(player1);
+	game.winPoint(player1);
+	expect(game.sets()).toEqual("1-0");
+})
+
+it("should win a match if score is 6-6 and player2 scores 7 points", function(){
+	player1.games = 6;
+	player2.games = 6;
+	game.winPoint(player2);
+	game.winPoint(player2);
+	game.winPoint(player2);
+	game.winPoint(player2);
+	game.winPoint(player2);
+	game.winPoint(player2);
+	game.winPoint(player2);
+	expect(game.sets()).toEqual("0-1");
+})
+
+
 }) // end of describe sets
 
+describe("winning a match", function(){
+
+it("should win a match if it wins two sets and the gender is female", function(){
+player1.sets=1;
+player1.games=5;
+game.winPoint(player1);
+game.winPoint(player1);
+game.winPoint(player1);
+game.winPoint(player1);
+game.sets();
+expect(game.match()).toEqual("1-0");
+})
+
+it("should not win a match if it wins two sets and the gender is male", function(){
+player3 = new Player("male");
+player4 = new Player("male");
+game1 = new Game(player3, player4);
+player3.sets=2;
+player3.games=5;
+game.winPoint(player3);
+game.winPoint(player3);
+game.winPoint(player3);
+game.winPoint(player3);
+game.sets();
+expect(game.match()).toEqual("0-0");
+})
+
+it("should win a match if it wins three sets and the gender is male", function(){
+player3 = new Player("male");
+player4 = new Player("male");
+game1 = new Game(player3, player4);
+player3.sets=2;
+player3.games=5;
+game1.winPoint(player3);
+game1.winPoint(player3);
+game1.winPoint(player3);
+game1.winPoint(player3);
+game1.sets();
+expect(game1.match()).toEqual("1-0");
+expect(player3.score).toEqual(0);
+})
+
+
+
+
+
+})
 
 
 }) //end of overarching describe
