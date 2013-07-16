36c9ab0b1a90298ca5783d9ace12baf37e7acf8b
Jeremy Goh <jg485host@gmail.com>
2013-07-16 21:09:55 +0100

    working version with games, tiebreaks,sets and matches. Needs to be refactored

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -1,23 +1,36 @@
+// need to call each function to update the number
+// maybe easier to have a display score function?
+
 function Game(p1, p2) {
 	this.player1 = p1;
 	this.player2 = p2;
 	FORMAT = ["0", "15", "30", "40", "ADV"];
 
 	this.winPoint = function(player){
-		player.score+=1;
-		if (this.player1.score === this.player2.score && this.player1.score >=3){
-			this.player1.score=3;
-			this.player2.score=3;
-		}
 
-		if((player1.score >= 4 && player1.score-player2.score >= 2) || (player2.score >= 4 && player2.score-player1.score >= 2) ) {
-			this.player1.score = 0;
-			this.player2.score = 0;
-			player.addGame();
-		}
 
-		return this.pointsConversion();
-	}
+		if(this.player1.games === 6 && this.player2.games === 6)
+		{
+			player.addTiebreak();			
+		}
+		else
+		{
+			player.score+=1;
+			if ((this.player1.score === this.player2.score) && this.player1.score >=3)
+				{
+				this.player1.score=3;
+				this.player2.score=3;
+				}
+
+			if((this.player1.score >= 4 && this.player1.score-player2.score >= 2) || (this.player2.score >= 4 && this.player2.score - this.player1.score >= 2) ) 
+				{
+				this.player1.score = 0;
+				this.player2.score = 0;
+				player.addGame();
+				}
+			}
+			return this.pointsConversion();
+		}
 
 	this.pointsConversion = function(){
 		return FORMAT[this.player1.score] + "-" + FORMAT[this.player2.score];
@@ -28,17 +41,70 @@ function Game(p1, p2) {
 	}
 
 	this.sets = function(){
-		if(player1.games === 6){
-			player1.games = 0;
-			player1.sets += 1;
+		if((this.player1.games === 6 && this.player2.games === 6) && player1.tiebreak >= 7 && (player1.tiebreak - player2.tiebreak >= 2))
+			{
+			this.player1.sets +=1
+			this.player1.games = 0;
+			this.player2.games = 0;
+			this.player1.tiebreak = 0;
+			this.player2.tiebreak = 0;
+			}	
+
+		if((this.player2.games === 6 && this.player1.games === 6) && player2.tiebreak >= 7 && (player2.tiebreak - player1.tiebreak >= 2))
+			{
+			this.player2.sets +=1
+			this.player1.games = 0;
+			this.player2.games = 0;
+			this.player1.tiebreak = 0;
+			this.player2.tiebreak = 0;
+			}			
+
+		if(this.player1.games >= 6 && (this.player1.games - this.player2.games)>=2){
+			this.player1.games = 0;
+			this.player2.games = 0;
+			this.player1.sets += 1;
 		}
 		
-		if(player2.games === 6){
-			player2.games = 0;
-			player2.sets += 1;
+		if(this.player2.games >= 6 && (this.player2.games - this.player1.games)>=2){
+			this.player2.games = 0;
+			this.player1.games = 0;
+			this.player2.sets += 1;
 		}
 
 		return this.player1.sets + "-" + this.player2.sets;
+	}
+
+	this.match = function(){
+		if(this.player1.gender === "female" && this.player1.sets === 2)
+			{this.player1.match +=1; 
+			this.player1.sets = 0;
+			this.player2.sets = 0;
+		}
+
+		if(this.player2.gender === "female" && this.player2.sets === 2)
+			{this.player2.match +=1; 
+			this.player1.sets = 0;
+			this.player2.sets = 0;
+		}
+
+		if(this.player1.gender === "male" && this.player1.sets === 3)
+			{this.player1.match +=1; 
+			this.player1.sets = 0;
+			this.player2.sets = 0;
+		}
+
+		if(this.player2.gender === "male" && this.player2.sets === 3)
+			{this.player2.match +=1; 
+			this.player1.sets = 0;
+			this.player2.sets = 0;
+		}
+
+		return this.player1.match + "-" + this.player2.match;
+	}
+
+	this.tiebreak = function(){
+		return this.player1.tiebreak + "-" + this.player2.tiebreak;
+
 	}
 
 }
--- public/javascripts/Player.js
+++ public/javascripts/Player.js
@@ -1,7 +1,10 @@
-function Player() {
+function Player(sex) {
 	this.score = 0;
 	this.games = 0;
 	this.sets = 0;
+  this.gender = sex; 
+  this.match = 0;
+  this.tiebreak = 0;
 }
 
 Player.prototype.addScore = function()  {
@@ -14,5 +17,13 @@ Player.prototype.addGame = function() {
 
 Player.prototype.addSet = function() {
 	this.sets += 1;
+}
+
+Player.prototype.addMatch = function() {
+  this.match += 1;
+}
+
+Player.prototype.addTiebreak = function(){
+  this.tiebreak += 1;
 }
 
